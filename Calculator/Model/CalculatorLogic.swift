35a66beef21f7d464d9ecdef3704c27540a7eb2e
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-08-01 16:33:37 -0700

    Files created and applied MVC

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,31 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by user208023 on 8/1/22.
+//  Copyright © 2022 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    var number: Double
+    
+    init(number: Double) {
+        self.number = number
+    }
+    
+    func calculate(symbol: String) -> Double? {
+        if symbol == "+/-" {
+            return number * -1
+        } else if symbol == "AC" {
+            return 0
+        } else if symbol == "%" {
+           return number * 0.01
+        }
+        
+        return nil
+    }
+    
+}
